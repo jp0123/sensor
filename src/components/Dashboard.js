ee0d8898fb26c9f5652427ab1e9098d957251d1a
Joseph Park <joseph.thomas.park@gmail.com>
2018-09-14 16:46:39 +1000

    Minor refactor - Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   render() {
     return (
       <div>
@@ -9,3 +8,5 @@ export default class Dashboard extends Component {
     )
   }
 }
+
+export default Dashboard;
