f940140ad776311227af005b771c5106c729cd7f
Joseph Park <joseph.thomas.park@gmail.com>
2018-09-14 18:14:44 +1000

    Add Sensor component

---
+++ src/components/Sensor.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sensor = data => {
+  const sensorIdData = data.data;
+
+  return (
+    <div>
+      <h1> Sensor </h1>
+    </div>
+  )
+}
+
+export default Sensor
