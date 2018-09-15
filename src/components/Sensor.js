494276e7a36e6eb06fb1e5e869fb14d29e8dd726
Joseph Park <joseph.thomas.park@gmail.com>
2018-09-15 12:12:42 +1000

    Add sensor id

--- src/components/Sensor.js
+++ src/components/Sensor.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Sensor = data => {
   const sensorDataById = data.data;
+  const sensorId = sensorDataById[0].id
   const numberOfSensorDataEntries = sensorDataById.length;
   const temperatureArray = [];
   sensorDataById.forEach(element => {
