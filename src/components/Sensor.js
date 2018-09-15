98e48d62f4aa4afb04273f91650e2bde60213172
Joseph Park <joseph.thomas.park@gmail.com>
2018-09-15 12:32:03 +1000

    Render average temperature

--- src/components/Sensor.js
+++ src/components/Sensor.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import calculateAverage from '../utility/calculateAverage';
 
 const Sensor = data => {
   const sensorDataById = data.data;
-  const sensorId = sensorDataById[0].id
-  const numberOfSensorDataEntries = sensorDataById.length;
+  const sensorId = `Sensor ${sensorDataById[0].id}`
   const temperatureArray = [];
   sensorDataById.forEach(element => {
     temperatureArray.push(element.temperature);
   })
+  const averageTemperature = calculateAverage(temperatureArray);
 
   return (
     <div>
-      <h1> Sensor </h1>
+      <h1>{sensorId}</h1>
+      <h2>Average Temperature: {averageTemperature}</h2>
     </div>
   )
 }
