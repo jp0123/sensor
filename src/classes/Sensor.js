e7ecb09e79efaa6c2af86f553bc2c140c200c2be
Joseph Park <joseph.thomas.park@gmail.com>
2018-09-21 17:46:02 +1000

    Create Sensor class

---
+++ src/classes/Sensor.js
@@ -0,0 +1,10 @@
+class Sensor {
+  constructor(id, average, median, mode) {
+    this.id = id;
+    this.average = average;
+    this.median = median;
+    this.mode = mode;
+  }
+}
+
+export default Sensor;
